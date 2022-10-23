039928b9095f7f32c8c94a97212e9f2331b87362
John Russell <jruss600@gmail.com>
2022-10-23 16:15:28 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React, { useEffect } from 'react'
+
+const App = () => {
+  return <>NBA Over Unders</>
+}
+
+export { App }
---
+++ src/interfaces/ITeam.ts
@@ -0,0 +1,6 @@
+export interface ITeam {
+  wins: number
+  gamesPlayed: number
+  line: number
+  pace: number
+}
