79e5b808ffec8f6727154e66c24d09f6d802d313
John Russell <jruss600@gmail.com>
2022-11-12 15:38:35 -0700

    border color change

--- src/components/styled-components/CollapsibleContainer.tsx
+++ src/components/styled-components/CollapsibleContainer.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const CollapsibleContainer = styled.div`
-  border: 1px solid blue;
+  border: 1px solid black;
   border-radius: 5px;
   margin: 2rem 4rem;
   padding: 1rem;
